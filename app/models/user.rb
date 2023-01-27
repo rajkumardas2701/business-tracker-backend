54f387770ab1696417127a991bee4d5ddd10ef2c
Rajkumar Das <rajkumardas2701@gmail.com>
2023-01-27 16:25:53 +0530

    update schema and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    require "securerandom"
+
+    has_secure_password
+
+    validates :name, presence: true
+    validates :password, presence: true
+    validates :number, presence: true, uniqueness: true
+
 end
