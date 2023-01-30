bd032661ee5c34e63e7e7761cb4456e3a4d7fe90
Rajkumar Das <rajkumardas2701@gmail.com>
2023-01-30 16:27:55 +0530

    Add Authentication controller

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,13 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_request
+
+  def login
+    @user = User.find_by_phone(params[:phone])
+    if @user&.authenticate(params[:password])
+      token = jwt_encode(user_id: @user.id)
+      render json: { token: token }, status: :ok
+    else
+      render json: {error: 'unauthoried' }, status: :unauthoried
+    end 
+  end
+end
