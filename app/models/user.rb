a88b052a798865998506ebfa767d2f231ec34f33
Rajkumar Das <rajkumardas2701@gmail.com>
2023-02-07 03:55:06 +0530

    add validator for email in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :password, presence: true
   validates :phone, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
