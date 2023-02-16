5708a0beb731f981f32ef5b2e042f685040b005f
Rajkumar Das <rajkumardas2701@gmail.com>
2023-02-16 22:50:09 +0530

    add validation on Financial Transaction model

--- app/models/financial_transaction.rb
+++ app/models/financial_transaction.rb
@@ -2,6 +2,12 @@ class FinancialTransaction < ApplicationRecord
   belongs_to :deal, optional: true
   belongs_to :user, optional: true
 
+  validates :date, presence: true
+  validates :send_receive, presence: true
+  validates :amount, presence: true
+  validates :action_by, presence: true
+  validates :remark, presence: true
+
   def filter_side_transactions
     FinancialTransaction.where(deal_id: nil)
   end
