0d0c8528a7737e5dd69f57fab131fdced2428b93
Rajkumar Das <rajkumardas2701@gmail.com>
2023-03-05 17:44:01 +0530

    change relation of deal with user in deal model

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,7 +1,7 @@
 class Deal < ApplicationRecord
   has_many :financial_transactions
   # has_many :users, through: :financial_transactions
-  # belongs_to :user
+  belongs_to :user
 
   validates :vehicle_date, presence: true
 end
