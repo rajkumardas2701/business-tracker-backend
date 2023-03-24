0f6212a6cd942dd623862a72d72ba54bf3dc7d49
Rajkumar Das <rajkumardas2701@gmail.com>
2023-03-24 15:17:56 +0530

    remove redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,16 +1,16 @@
-require 'redis'
+# require 'redis'
 
-redis_host: ENV.fetch('REDIS_HOST')
-redis_port: ENV.fetch('REDIS_PORT')
-redis_password: ENV.fetch('REDIS_ACCESS_KEY')
+# redis_host = ENV.fetch('REDIS_HOST')
+# redis_port = ENV.fetch('REDIS_PORT')
+# redis_password = ENV.fetch('REDIS_ACCESS_KEY')
 
-redis = Redis.new(
-  host: redis_host,
-  port: redis_port,
-  passowrd: redis_password,
-  ssl: false,
-  reconnect_attempts: 5,
-  reconnect_delay: 1
-)
+# redis = Redis.new(
+#   host: redis_host,
+#   port: redis_port,
+#   passowrd: redis_password,
+#   ssl: false,
+#   reconnect_attempts: 5,
+#   reconnect_delay: 1
+# )
 
-Redis.current = redis
+# Redis.current = redis
