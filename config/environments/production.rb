25944b7a5c7a3a42f7081d1b7ef021429efcf2ee
Rajkumar Das <rajkumardas2701@gmail.com>
2023-03-27 22:00:37 +0530

    add new frontend origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  config.action_cable.allowed_request_origins = [ "https://elegant-cascaron-fb6a74.netlify.app" ]
+  config.action_cable.allowed_request_origins = [ "https://elegant-cascaron-fb6a74.netlify.app", "https://businesstrackerapp.azurewebsites.net" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://elegant-cascaron-fb6a74.netlify.app"
+    origins "https://elegant-cascaron-fb6a74.netlify.app", "https://businesstrackerapp.azurewebsites.net"
 
     resource "*",
       headers: :any,
