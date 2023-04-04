348165f43d1c99a9b08df3653ff63689eb8a98f3
Rajkumar Das <rajkumardas2701@gmail.com>
2023-04-04 15:37:01 +0530

    add mailer styles

--- app/mailers/transactions_mailer.rb
+++ app/mailers/transactions_mailer.rb
@@ -1,6 +1,6 @@
 class TransactionsMailer < ApplicationMailer
   def new_transaction_email
     @tx = params[:tx]
-    mail(to: ENV.fetch('gmail', nil), subject: 'New Transaction posted')
+    mail(to: ENV.fetch('gmail', nil), subject: "New Transaction of amount #{@tx.amount}")
   end
 end
